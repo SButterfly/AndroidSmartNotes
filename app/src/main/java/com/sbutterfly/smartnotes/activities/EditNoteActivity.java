b62177b5fa14e9b6f099a9e1ea5de53985ef8548
Sergei Ermakov <sergeiermakof@gmail.com>
2016-04-13 21:02:15 +0400

    Added multilines to edit note activity

--- app/src/main/java/com/sbutterfly/smartnotes/activities/EditNoteActivity.java
+++ app/src/main/java/com/sbutterfly/smartnotes/activities/EditNoteActivity.java
@@ -1,6 +1,7 @@
 package com.sbutterfly.smartnotes.activities;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -122,8 +123,9 @@ public class EditNoteActivity extends AppCompatActivity {
                     if (note.getId() != Note.INVALID_ID) {
                         notesAccessObject.deleteNote(note);
                     }
-                    // TODO you should navigate to first activity
-                    finish();
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
                 }
             }
         };
